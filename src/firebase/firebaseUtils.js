9869311eebdcb42385e0a3f554620affa518e2e6
Ahmad Al-waili <aalwailia@gmail.com>
2021-12-10 02:19:35 +0000

    read me update

--- src/firebase/firebaseUtils.js
+++ src/firebase/firebaseUtils.js
@@ -1,6 +1,8 @@
-import firebase from "firebase/compat/app"
-import "firebase/firestore"
-import "firebase/auth"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+require('dotenv').config()
+
 
 const { REACT_APP_FIREBASE_API_KEY, REACT_APP_FIREBASE_AUTH_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_MESSAGING_SENDER_ID, REACT_APP_FIREBASE_APP_ID, REACT_APP_FIREBASE_MEASUREMEMT_ID } = process.env;
 
